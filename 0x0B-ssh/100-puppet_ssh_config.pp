ac3a2673a4dbabb3e4cf5d9e21c00c6613bcd0c7
Amanbel <amanuel11belachew@gmail.com>
2023-07-09 02:35:30 +0300

    config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # setup your ssh config file with puppet
-file line { 'Turn off password auth':
+file_line { 'Turn off password auth':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    passwordAuthentication no',
   }
-file line { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => '    IdentityFile ~/.ssh/school',
