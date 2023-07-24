f098e0a78785e4654458a47b43a5892bfd4a1c34
Amanbel <amanuel11belachew@gmail.com>
2023-07-24 20:22:14 +0300

    fetchData

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,15 +4,20 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
+    if len(argv) < 2:
+        exit()
 
     i = 0
+
     res = requests.get("https://jsonplaceholder.\
 typicode.com/todos?userId={}".format(argv[1]))
     userRes = requests.get("https://jsonplaceholder.\
 typicode.com/users/{}".format(argv[1]))
+
     for obj in res.json():
         if (obj['completed'] is True):
             i += 1
+
     print("Employee {} is done with \
 tasks({}/{}):".format(userRes.json()['name'], i, len(res.json())))
 
