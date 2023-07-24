58ece67ccde1ceffd50a8fab1e81c0ada530db66
Amanbel <amanuel11belachew@gmail.com>
2023-07-24 21:57:10 +0300

    fetch_to_csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        exit()
+
+    userRes = requests.get("https://jsonplaceholder.\
+typicode.com/users/{}".format(argv[1]))
+    todoRes = requests.get("https://jsonplaceholder.\
+typicode.com/todos?userId={}".format(argv[1]))
+
+    with open('{}.csv'.format(argv[1]), 'w') as csvfile:
+        thewriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        name = userRes.json()["username"]
+        for obj in todoRes.json():
+            thewriter.writerow([argv[1], name, obj["completed"], obj["title"]])
