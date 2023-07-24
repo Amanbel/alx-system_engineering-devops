3cb4f2a98c03a4373c2f1e1fcb580b4710496b63
Amanbel <amanuel11belachew@gmail.com>
2023-07-24 20:06:47 +0300

    fetchData

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    i = 0
+    res = requests.get("https://jsonplaceholder.\
+typicode.com/todos?userId={}".format(argv[1]))
+    userRes = requests.get("https://jsonplaceholder.\
+typicode.com/users/{}".format(argv[1]))
+    for obj in res.json():
+        if (obj['completed'] is True):
+            i += 1
+    print("Employee {} is done with \
+tasks({}/{}):".format(userRes.json()['name'], i, len(res.json())))
+
+    for obj in res.json():
+        if (obj['completed'] is True):
+            print("\t {}".format(obj['title']))
