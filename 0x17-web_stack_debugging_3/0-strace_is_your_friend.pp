0c5a9f28f59fef018508fc9c155a8faee38cb125
Amanbel <amanuel11belachew@gmail.com>
2023-08-09 23:13:44 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
-# fixes bad php extension
-
+# fixes bad php extension in a wordpress bug
 exec { 'fix_error':
   command  => 'sed -i "s|.phpp|.php|g" /var/www/html/wp-settings.php',
   path     => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
