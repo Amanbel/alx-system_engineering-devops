642a27be3f41d7c81df9a3029db76ac42b62f04d
Amanbel <amanuel11belachew@gmail.com>
2023-08-09 23:07:14 +0300

    fix:strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fixes bug
+# fixes bad php extension
 
-exec { 'fix-sordpress':
+exec { 'fix-wordpress':
   command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php,
   path     => '/usr/local/bin/:/bin/'
 }
